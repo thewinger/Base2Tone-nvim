6127f3620111888b4b77b1521f2f18e7c526fc7f
thewinger <winger.87@gmail.com>
2025-02-11 10:49:04 +0100

    rosepine

---
+++ colors/base2tone_rosepine_dark.vim
@@ -0,0 +1,4 @@
+lua << EOF
+local base2tone_rosepine_dark = require("base2tone_rosepine_dark")
+base2tone_rosepine_dark.setup({})
+EOF
---
+++ lua/base2tone_rosepine_dark/init.lua
@@ -0,0 +1,19 @@
+local M = {}
+local theme = require("base2tone_rosepine_dark.theme")
+
+M.setup = function()
+	vim.cmd("hi clear")
+
+	vim.o.background = "dark"
+	if vim.fn.exists("syntax_on") then
+		vim.cmd("syntax reset")
+	end
+
+	vim.o.termguicolors = true
+	vim.g.colors_name = "base2tone_rosepine_dark"
+
+	theme.set_highlights()
+end
+
+return M
+
