0f7dd4f02251818b576b2d77ec486c6f56d913ce
thewinger <winger.87@gmail.com>
2025-03-05 08:43:40 +0100

    Update

--- lua/base2tone_tailwind_dark/palette.lua
+++ lua/base2tone_tailwind_dark/palette.lua
@@ -1,8 +1,8 @@
 local colors = {
-	B2T_A0 = "#24242e",
-	B2T_A1 = "#333342",
+	bg_dark = "#24242e",
+	bg_med = "#333342",
 	B2T_A2 = "#515167",
-	B2T_A3 = "#5b5b76",
+	comment = "#5b5b76",
 	B2T_A4 = "#737391",
 	B2T_A5 = "#8a8aa3",
 	B2T_A6 = "#a1a1b5",
@@ -10,28 +10,24 @@ local colors = {
 	B2T_B0 = "#5151e6",
 	B2T_B1 = "#6363ee",
 	B2T_B2 = "#7676f4",
-	B2T_B3 = "#767693",
+	fg_dark = "#767693",
 	B2T_B4 = "#8a8aad",
-	B2T_B5 = "#aaaaca",
+	fg_med = "#aaaaca",
 	B2T_B6 = "#cecee3",
-	B2T_B7 = "#ebebff",
+	fg_light = "#ebebff",
+
 	B2T_C0 = "#7b736f",
 	B2T_C1 = "#8e8580",
 	B2T_C2 = "#a09792",
 	B2T_C3 = "#b1a9a5",
-	B2T_C4 = "#c3bbb7", -- not used
 	B2T_C5 = "#d8cfcb",
-	B2T_C6 = "#eae4e1", -- not used
-	B2T_C7 = "#fbf9f9", -- not used
-	B2T_D0 = "#8b0836",
-	B2T_D1 = "#a50036",
-	B2T_PINK_ALT = "#f6339a",
-	B2T_PINK_MAIN = "#fb64b6",
-	B2T_ERROR = "#FB7185",
-	B2T_WARN = "#FBBF24",
-	B2T_OK = "#34D399",
-	B2T_HINT = "#38BDF8",
-	B2T_INFO = "#7DD3FC",
+	primary_alt = "#f6339a",
+	primary_main = "#fb64b6",
+	error = "#FB7185",
+	warning = "#FBBF24",
+	success = "#34D399",
+	hint = "#38BDF8",
+	info = "#7DD3FC",
 }
 
 return colors
